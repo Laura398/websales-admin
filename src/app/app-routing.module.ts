e3b76bee592bf1fc09e63795cd0b4b76f7e24620
Laura Mathieu <lauramathieu.git@gmail.com>
2022-04-25 16:29:43 +0200

    Start project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AuthComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'users', component: UsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ProductsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
